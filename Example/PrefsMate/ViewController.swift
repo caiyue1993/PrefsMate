58f4b871175cd130f10eb6a3c3a3735b7ccb710e
Yue Cai <>
2017-09-26 12:01:06 +0800

    Initial commit

---
+++ Example/PrefsMate/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PrefsMate
+//
+//  Created by caiyue1993 on 09/26/2017.
+//  Copyright (c) 2017 caiyue1993. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
