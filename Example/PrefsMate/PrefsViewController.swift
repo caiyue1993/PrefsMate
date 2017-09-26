1abd9aecbaafb7d9815ebbd5debd11d2388322fe
Yue Cai <caiyue@caiyuedeMBP-3.shanbay>
2017-09-26 16:35:51 +0800

    add / remove files

--- Example/PrefsMate/PrefsViewController.swift
+++ Example/PrefsMate/PrefsViewController.swift
@@ -7,18 +7,18 @@
 //
 
 import UIKit
+import PrefsMate
 
 class ViewController: UIViewController {
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .white
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 }
 
---
+++ PrefsMate.swift
@@ -0,0 +1,16 @@
+//
+//  Mate.swift
+//  PrefsMate
+//
+//  Created by 蔡越 on 26/09/2017.
+//
+
+import Foundation
+
+public typealias PrefsTableView = UITableView
+
+open class Mate {
+    
+    public static create
+    
+}
