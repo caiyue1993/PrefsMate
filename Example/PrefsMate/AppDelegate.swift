f846bb3ccce1bd2ecd6c8ac4be795ffbae75f02b
Yue Cai <caiyue@caiyuedeMBP-3.shanbay>
2017-09-28 15:17:18 +0800

    Adjust root view controller in demo

--- Example/PrefsMate/AppDelegate.swift
+++ Example/PrefsMate/AppDelegate.swift
@@ -15,10 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = UINavigationController(rootViewController: HomeViewController())
+        window?.rootViewController = UINavigationController(rootViewController: PrefsViewController(with: Bundle.main.url(forResource: "Prefs", withExtension: "plist")!))
         window?.makeKeyAndVisible()
         return true
     }
-
+    
 }
 
